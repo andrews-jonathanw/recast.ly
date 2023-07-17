9b1b24dcb0be04a679c9db3483add15c31d70461
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-17 16:32:09 -0500

    Passing tests for videoList, VideoListEntry, and VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
+
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -7,10 +14,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> <VideoPlayer /></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em> <VideoList /></h5></div>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,32 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
+import VideoPlayer from './VideoPlayer.js';
+
+
+
+var VideoList = ({videos}) => {
+
+  return (
+
+    <div className="video-list">
+      {videos.map((video) => (
+        <VideoListEntry video={video}/>
+
+      )
+
+      )}
+
+    </div>
+  );
+
+
+  // <div className="video-list">
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +37,5 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
